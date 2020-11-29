11dec346430fb4f6e7c342ba9f0758ecf3bfb8a1
Bryan Harper <brharper@corelogic.com>
2020-11-29 14:52:47 -0600

    Tidy up build.fsx.

--- build.fsx
+++ build.fsx
@@ -7,78 +7,127 @@ open Fake.DotNet
 open Fake.IO
 open Farmer
 open Farmer.Builders
+open Fake.JavaScript
 
 Target.initEnvironment ()
 
+// Constants
 let sharedPath = Path.getFullName "./src/Shared"
 let serverPath = Path.getFullName "./src/Server"
 let deployDir = Path.getFullName "./deploy"
 let sharedTestsPath = Path.getFullName "./tests/Shared"
 let serverTestsPath = Path.getFullName "./tests/Server"
 
-let npm args workingDir =
-    let npmPath =
-        match ProcessUtils.tryFindFileOnPath "npm" with
+// Helpers
+let buildRawCmd cmd args workingDir =
+    let path =
+        match ProcessUtils.tryFindFileOnPath cmd with
         | Some path -> path
         | None ->
-            "npm was not found in path. Please install it and make sure it's available from your path. " +
-            "See https://safe-stack.github.io/docs/quickstart/#install-pre-requisites for more info"
+            sprintf "%s was not found in path. Please install and add it to path." cmd
             |> failwith
 
-    let arguments = args |> String.split ' ' |> Arguments.OfArgs
-
-    Command.RawCommand (npmPath, arguments)
+    RawCommand(path, args |> String.split ' ' |> Arguments.OfArgs)
     |> CreateProcess.fromCommand
     |> CreateProcess.withWorkingDirectory workingDir
+
+let execRawHandled cmd args workingDir =
+    buildRawCmd cmd args workingDir
     |> CreateProcess.ensureExitCode
     |> Proc.run
     |> ignore
 
+let execRaw cmd args workingDir =
+    buildRawCmd cmd args workingDir |> Proc.run
+
 let dotnet cmd workingDir =
-    let result = DotNet.exec (DotNet.Options.withWorkingDirectory workingDir) cmd ""
-    if result.ExitCode <> 0 then failwithf "'dotnet %s' failed in %s" cmd workingDir
+    let result =
+        DotNet.exec (DotNet.Options.withWorkingDirectory workingDir) cmd ""
 
-Target.create "Clean" (fun _ -> Shell.cleanDir deployDir)
+    if result.ExitCode <> 0
+    then failwithf "'dotnet %s' failed in %s" cmd workingDir
 
-Target.create "InstallClient" (fun _ -> npm "install" ".")
+let printSection msg =
+    Trace.traceLine ()
+    Trace.tracefn "%s" msg
+    Trace.traceLine ()
 
-Target.create "Bundle" (fun _ ->
+// Targets
+Target.create "Clean"
+<| fun _ ->
+    "Clean Deploy Directory" |> printSection
+    Shell.cleanDir deployDir
+
+Target.create "InstallClient"
+<| fun _ ->
+    "Installing Client" |> printSection
+    Npm.install id
+
+Target.create "Bundle"
+<| fun _ ->
+    "Bundling App" |> printSection
     dotnet (sprintf "publish -c Release -o \"%s\"" deployDir) serverPath
-    npm "run build" "."
-)
+    Npm.run "build" id
 
-Target.create "Azure" (fun _ ->
-    let web = webApp {
-        name "philectrosophy"
-        zip_deploy "deploy"
-    }
-    let deployment = arm {
-        location Location.WestEurope
-        add_resource web
-    }
+Target.create "Azure"
+<| fun _ ->
+    "Deploying to Azure" |> printSection
+    let appName = "philectrosophy"
+
+    let web =
+        webApp {
+            name appName
+            zip_deploy "deploy"
+        }
+
+    let deployment =
+        arm {
+            location Location.WestEurope
+            add_resource web
+        }
 
     deployment
-    |> Deploy.execute "philectrosophy" Deploy.NoParameters
+    |> Deploy.execute appName Deploy.NoParameters
     |> ignore
-)
 
-Target.create "Run" (fun _ ->
+
+Target.create "Run"
+<| fun _ ->
+    "Starting App" |> printSection
     dotnet "build" sharedPath
-    [ async { dotnet "watch run" serverPath }
-      async { npm "run start" "." } ]
+    [
+        async { dotnet "watch run" serverPath }
+        async { Npm.run "start" id }
+    ]
     |> Async.Parallel
     |> Async.RunSynchronously
     |> ignore
-)
 
-Target.create "RunTests" (fun _ ->
+Target.create "BuildSharedTests"
+<| fun _ ->
+    "Building Shared Tests" |> printSection
     dotnet "build" sharedTestsPath
-    [ async { dotnet "watch run" serverTestsPath }
-      async { npm "run test:live" "." } ]
+
+Target.create "Test"
+<| fun _ ->
+    "Run Server Tests" |> printSection
+    dotnet "run" serverTestsPath
+    "Run Client Tests" |> printSection
+    Npm.run "test" id
+
+Target.create "LiveTest"
+<| fun _ ->
+    "Running Live Tests" |> printSection
+    [
+        async { dotnet "watch run" serverTestsPath }
+        async { Npm.run "test:live" id }
+    ]
     |> Async.Parallel
     |> Async.RunSynchronously
     |> ignore
-)
+
+Target.create "List"
+<| fun _ -> Target.listAvailable ()
 
 open Fake.Core.TargetOperators
 
@@ -87,12 +136,16 @@ open Fake.Core.TargetOperators
     ==> "Bundle"
     ==> "Azure"
 
-"Clean"
-    ==> "InstallClient"
-    ==> "Run"
+"Clean" ==> "InstallClient" ==> "Run"
 
 "Clean"
-    ==> "InstallClient"
-    ==> "RunTests"
+==> "InstallClient"
+==> "BuildSharedTests"
+==> "Test"
 
-Target.runOrDefaultWithArguments "Bundle"
+"Clean"
+==> "InstallClient"
+==> "BuildSharedTests"
+==> "LiveTest"
+
+Target.runOrDefault "List"
