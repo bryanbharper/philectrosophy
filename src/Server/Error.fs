04d3077a76bb06a0878a04ad51a4c545e2fb6fee
Bryan Harper <brharper@corelogic.com>
2020-12-17 15:27:31 -0600

    Add Logging
    [#176213735]

---
+++ src/Server/Error.fs
@@ -0,0 +1,18 @@
+﻿module Error
+
+open System
+open Fable.Remoting.Server
+open Microsoft.AspNetCore.Http
+open Giraffe.SerilogExtensions
+
+let handler (ex: Exception) (routeInfo: RouteInfo<HttpContext>) =
+
+    let contextLogger = routeInfo.httpContext.Logger()
+
+    let errorMsgTemplate =
+        "Error occured while invoking {MethodName} at {RoutePath}"
+    contextLogger.Error(ex, errorMsgTemplate, routeInfo.methodName, routeInfo.path)
+
+    // See https://zaid-ajaj.github.io/Fable.Remoting/src/error-handling.html
+    match ex with
+    | _ -> Ignore
--- src/Server/Server.fs
+++ src/Server/Server.fs
@@ -4,9 +4,11 @@ open Data
 open Fable.Remoting.Server
 open Fable.Remoting.Giraffe
 open File
+open Giraffe.SerilogExtensions
 open Microsoft.Extensions.DependencyInjection
 open Saturn
 
+open Serilog
 open Shared
 
 let configureServices (services : IServiceCollection) =
@@ -19,16 +21,30 @@ let webApp =
     Remoting.createApi()
     |> Remoting.withRouteBuilder Route.builder
     |> Remoting.fromReader BlogApi.blogApiReader
+    |> Remoting.withErrorHandler Error.handler
     |> Remoting.buildHttpHandler
+
+let webAppWithLogging = SerilogAdapter.Enable(webApp)
+
+Log.Logger <-
+    LoggerConfiguration()
+      // this helps Serilog deserialize the fsharp types like unions/records
+      .Destructure.FSharpTypes()
+      // use package Serilog.Sinks.Console
+      // https://github.com/serilog/serilog-sinks-console
+      .WriteTo.Console()
+      // add more sinks etc.
+      .CreateLogger()
 
 let app =
     application {
         url "http://0.0.0.0:8085"
-        use_router webApp
+        use_router webAppWithLogging
         service_config configureServices
         memory_cache
         use_static "public"
         use_gzip
     }
+
 
 run app
